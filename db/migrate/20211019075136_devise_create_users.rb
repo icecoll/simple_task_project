fab8253a3aa780b3c61cd5ecedf8825956ce785e
lianming <lianming@lianmingdeMacBook-Pro.local>
2021-10-19 21:06:49 +0800

    fix code style

--- db/migrate/20211019075136_devise_create_users.rb
+++ db/migrate/20211019075136_devise_create_users.rb
@@ -3,12 +3,12 @@
 class DeviseCreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :username,           null: false, default: ""
+      t.string :username, null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       t.timestamps null: false
     end
 
-    add_index :users, :username,             unique: true
+    add_index :users, :username, unique: true
   end
 end
