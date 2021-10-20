062b107845a9a8c3492d5bf1c7543c7f9cddc377
lianming <lianming@lianmingdeMacBook-Pro.local>
2021-10-20 08:48:30 +0800

    password validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable
 
   validates :username, uniqueness: true, presence: true, length: {minimum: 4, maximum: 16}
-  validates :username, format: {with: /\A[a-zA-Z0-9]*\z/, messages: "only allows alphanumeric character"}
+  validates :username, format: {with: /\A[a-zA-Z0-9]*\z/, messages: "only allows alphanumeric character"} # todo I18n
+
+  validates_presence_of :password, if: :password_required?
+  validates_confirmation_of :password, if: :password_required?
+  validates_length_of :password, within: 8..16, allow_blank: true # todo: use devise config instead hardcode length range
+  validates :password, format: {
+    with: /\A(?:(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*)\z/,
+    messages: "should contains at least 1 upper case, 1 lower case, 1 number" # todo I18n
+  }
+
+  protected
+
+  # Checks whether a password is needed or not. For validations only.
+  # Passwords are always required if it's a new record, or if the password
+  # or confirmation are being set somewhere.
+  def password_required?
+    !persisted? || !password.nil? || !password_confirmation.nil?
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe User, type: :model do
     it { should validate_uniqueness_of(:username).case_insensitive }
     it { should validate_length_of(:username).is_at_least(4).is_at_most(16) }
 
+    it { should validate_presence_of(:password) }
+    it { should validate_confirmation_of(:password) }
+    it { should validate_length_of(:password).is_at_least(8).is_at_most(16) }
+
     it "should be invalid when username contains special character" do
       user = User.new(username: "fooo$")
       expect(user.valid?).to be_falsy
@@ -13,7 +17,31 @@ RSpec.describe User, type: :model do
     end
 
     it "should be valid when username only contains alphanumeric characters" do
-      expect(User.new(username: "foo123").valid?).to be_truthy
+      user = User.new(username: "foo123")
+      expect(user.errors.where(:username).present?).to be_falsy
+    end
+
+    it "should be invalid when password not contains upper case character" do
+      user = User.new(password: "foo123")
+      expect(user.valid?).to be_falsy
+      expect(user.errors.where(:password).last.options[:messages]).to eq("should contains at least 1 upper case, 1 lower case, 1 number")
+    end
+
+    it "should be invalid when password not contains lower case character" do
+      user = User.new(password: "FOO123")
+      expect(user.valid?).to be_falsy
+      expect(user.errors.where(:password).last.options[:messages]).to eq("should contains at least 1 upper case, 1 lower case, 1 number")
+    end
+
+    it "should be invalid when password not contains number" do
+      user = User.new(password: "FOOoo")
+      expect(user.valid?).to be_falsy
+      expect(user.errors.where(:password).last.options[:messages]).to eq("should contains at least 1 upper case, 1 lower case, 1 number")
+    end
+
+    it "should be valid when password contains upper case, lower case and number" do
+      user = User.new(password: "FOOoo1")
+      expect(user.errors.where(:password).present?).to be_falsy
     end
   end
 end
