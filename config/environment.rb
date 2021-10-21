ff002d8192018505b42fb692d9b123a30a1df797
lianming <lianming@lianmingdeMacBook-Pro.local>
2021-10-21 18:27:04 +0800

    fix: login failed message

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Block Rails Wrap a field_with_errors div when validation failed
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
